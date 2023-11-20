6f0dc293575dc60928d846773a323588bf28b603
kumail imran <kumail2306@gmail.com>
2023-11-20 14:10:42 +0000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,9 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <p>hello</p>
+    </main>
+  );
 }
